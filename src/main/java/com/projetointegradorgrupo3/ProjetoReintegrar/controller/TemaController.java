44df8311fa1e977cc9469eecb424f4aa4162cd73
Gazzara <Gian.Gazzara@storaenso.com>
2021-09-01 16:09:49 -0300

    deploy

---
+++ src/main/java/com/projetointegradorgrupo3/ProjetoReintegrar/controller/TemaController.java
@@ -0,0 +1,53 @@
+package com.projetointegradorgrupo3.ProjetoReintegrar.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.projetointegradorgrupo3.ProjetoReintegrar.model.Tema;
+import com.projetointegradorgrupo3.ProjetoReintegrar.repository.TemaRepository;
+
+@RestController
+@RequestMapping("/tema")
+@CrossOrigin("*")
+public class TemaController {
+
+	@Autowired
+	private TemaRepository repository;
+	
+	@GetMapping
+    public ResponseEntity<List<Tema>> findAll(){
+        return ResponseEntity.ok(repository.findAll());
+    }
+	
+	@GetMapping("/tema/{tema}")
+    public ResponseEntity<List<Tema>> GetByDescricao(@PathVariable String tema){
+        return ResponseEntity.ok(repository.findAllByDescricaoContainingIgnoreCase(tema));
+    }
+	
+	@PostMapping
+    public ResponseEntity<Tema> post (@RequestBody Tema tema){
+        return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(tema));
+    }
+
+    @PutMapping
+    public ResponseEntity<Tema> put (@RequestBody Tema tema){
+        return ResponseEntity.status(HttpStatus.OK).body(repository.save(tema));
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable long id) {
+        repository.deleteById(id);
+    }
+}
---
+++ src/main/java/com/projetointegradorgrupo3/ProjetoReintegrar/model/Usuario.java
@@ -0,0 +1,199 @@
+package com.projetointegradorgrupo3.ProjetoReintegrar.model;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.sun.istack.NotNull;
+
+@Entity
+@Table (name = "usuario")
+public class Usuario {
+	
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotBlank
+	@Size (min = 1, max = 100)
+	private String nome;
+	
+//	@Column(name = "tb_nascimento")
+//	@JsonFormat(pattern = "yyyy-MM-dd")
+//	private LocalDate dataNascimento ;
+	
+	@NotBlank
+	@Size (min = 5, max = 20)
+	private String genero;
+	
+	@NotBlank
+	@Size (min = 5, max = 100)
+	private String email;
+	
+	//@Pattern TODO pesquisar depois
+	private String telefone;
+	
+	@NotBlank
+	@Size (min = 6)
+	private String senha;
+	
+//	@NotBlank
+//	@Size (min=5,max=100)
+//	private String periodo;
+	
+	@Min (0)
+	private double pretensaoSalarial;
+	
+	private String foto;
+	
+	private String tipo;
+
+	
+	@ManyToOne
+	@JsonIgnoreProperties ("usuarios")
+	private Categoria categoria;
+	
+	@OneToMany (mappedBy = "usuario", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties ("usuario")
+	private List<Postagem> postagens;
+	
+	public Usuario (long id, String nome , LocalDate dataNascimento, String genero , String email
+			, String telefone , String senha , String periodo , double pretensaoSalarial) {
+		
+		this.id = id;
+		this.nome = nome;
+//		this.dataNascimento = dataNascimento;
+		this.genero = genero;
+		this.email = email;
+		this.telefone = telefone;
+		this.senha = senha;
+//		this.periodo = periodo;
+		this.pretensaoSalarial = pretensaoSalarial;
+	}
+	
+	public Usuario () {
+		
+	}
+	
+	
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public List<Postagem> getPostagens() {
+		return postagens;
+	}
+
+	public void setPostagens(List<Postagem> postagens) {
+		this.postagens = postagens;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+//	public String getPeriodo() {
+//		return periodo;
+//	}
+//
+//	public void setPeriodo(String periodo) {
+//		this.periodo = periodo;
+//	}
+
+	public double getPretensaoSalarial() {
+		return pretensaoSalarial;
+	}
+
+	public void setPretensaoSalarial(double pretensaoSalarial) {
+		this.pretensaoSalarial = pretensaoSalarial;
+	}
+
+//	public LocalDate getDataNascimento() {
+//		return dataNascimento;
+//	}
+//
+//	public void setDataNascimento(LocalDate dataNascimento) {
+//		this.dataNascimento = dataNascimento;
+//	}
+	
+	
+}
